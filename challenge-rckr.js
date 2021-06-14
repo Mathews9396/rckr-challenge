3e69a2b490ffdcaa469193af6b1d1ad5660d6fcb
Mathews Mathews <mathewsmathews95@gmail.com>
2021-06-14 16:34:15 +0530

    optimized the variables

--- challenge-rckr.js
+++ challenge-rckr.js
@@ -6,24 +6,21 @@ fs.readFile('./data/countriesV2.json', 'utf-8', (err, data) => {
     if (err) {
         throw err;
     }
-
     // parse JSON object
     const countries = JSON.parse(data.toString());
 
-    var i = 0;
-    var count = 0;
-    var emptyLatLong=0;
-    countriesLength = countries.length;
-    sortedDataEntries = [];
+    var i = 0, count = 0, emptyLatLong = 0;
+    const countriesLength = countries.length;
+    var sortedDataEntries = [];
 
     for (i = 0; i < countriesLength && count < 20; i++) {
         if (countries[i].population >= 57100) {
             sortedDataEntries.push([countries[i].name, countries[i].population, countries[i].latlng]);
             count++;
-            }
         }
-console.log('no of empty lat and long:  ',emptyLatLong);
-    var j, lat1, lat2, long1, long2, totalKM = 0;
+    }
+
+    var j = 0, lat1 = 0, lat2 = 0, long1 = 0, long2 = 0, totalKM = 0;
 
     //Evaluvating the sum of lines between all countries
 
